b73d429a57d2dfc57036646ea99cbb1cdf9cc4ce
SaraUlan <220872@astanait.edu.kz>
2024-02-19 20:17:26 +0600

    Created contact_repository.go

---
+++ services/contact/internal/repository/contact_repository.go
@@ -0,0 +1,18 @@
+// services/contact/internal/repository/contact_repository.go
+
+package repository
+
+import "github.com/SaraUlan/SaraUlan-03/services/contact/internal/domain"
+
+type ContactRepository interface {
+	CreateContact(contact *domain.Contact) (*domain.Contact, error)
+	GetContactByID(contactID int) (*domain.Contact, error)
+	UpdateContact(contact *domain.Contact) (*domain.Contact, error)
+	DeleteContact(contactID int) error
+
+	CreateGroup(group *domain.Group) (*domain.Group, error)
+
+	GetGroupByID(groupID int) (*domain.Group, error)
+
+	AddContactToGroup(contactID, groupID int) error
+}
