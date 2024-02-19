c0d99c88af227297bd67a76d285be8b68954717f
SaraUlan <220872@astanait.edu.kz>
2024-02-19 20:15:43 +0600

    Created contact_usecase.go

---
+++ services/contact/internal/usecase/contact_usecase.go
@@ -0,0 +1,20 @@
+// services/contact/internal/usecase/contact_usecase.go
+
+package usecase
+
+import (
+	"github.com/SaraUlan/SaraUlan-03/services/contact/internal/domain"
+)
+
+type ContactUseCase interface {
+	CreateContact(contact *domain.Contact) (*domain.Contact, error)
+	GetContactByID(contactID int) (*domain.Contact, error)
+	UpdateContact(contact *domain.Contact) (*domain.Contact, error)
+	DeleteContact(contactID int) error
+
+	CreateGroup(group *domain.Group) (*domain.Group, error)
+
+	GetGroupByID(groupID int) (*domain.Group, error)
+
+	CreateAndAddContactToGroup(contact *domain.Contact, groupID int) (*domain.Contact, error)
+}
