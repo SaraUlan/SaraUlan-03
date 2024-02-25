d57377be31628df0d9564d4d93679c96870ec730
SaraUlan <220872@astanait.edu.kz>
2024-02-25 20:55:52 +0600

    4assig task2

--- services/contact/internal/delivery/contact_delivery_impl.go
+++ services/contact/internal/delivery/contact_delivery_impl.go
@@ -13,39 +13,30 @@ func NewContactDelivery(useCase usecase.ContactUseCase) ContactDelivery {
 	return &ContactDeliveryImpl{UseCase: useCase}
 }
 
-
-
 func (cd *ContactDeliveryImpl) CreateContact(contact *domain.Contact) (*domain.Contact, error) {
-
-	return contact, nil
+	return cd.UseCase.CreateContact(contact)
 }
 
 func (cd *ContactDeliveryImpl) GetContactByID(contactID int) (*domain.Contact, error) {
-
-	return nil, nil
+	return cd.UseCase.GetContactByID(contactID)
 }
 
 func (cd *ContactDeliveryImpl) UpdateContact(contact *domain.Contact) (*domain.Contact, error) {
-
-	return contact, nil
+	return cd.UseCase.UpdateContact(contact)
 }
 
 func (cd *ContactDeliveryImpl) DeleteContact(contactID int) error {
-
-	return nil
+	return cd.UseCase.DeleteContact(contactID)
 }
 
 func (cd *ContactDeliveryImpl) CreateGroup(group *domain.Group) (*domain.Group, error) {
-
-	return group, nil
+	return cd.UseCase.CreateGroup(group)
 }
 
 func (cd *ContactDeliveryImpl) GetGroupByID(groupID int) (*domain.Group, error) {
-
-	return nil, nil
+	return cd.UseCase.GetGroupByID(groupID)
 }
 
 func (cd *ContactDeliveryImpl) CreateAndAddContactToGroup(contact *domain.Contact, groupID int) (*domain.Contact, error) {
-
-	return contact, nil
+	return cd.UseCase.CreateAndAddContactToGroup(contact, groupID)
 }
