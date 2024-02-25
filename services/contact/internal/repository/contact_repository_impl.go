d5334598f4d8a09b02a77783fd3a6239331bb492
SaraUlan <220872@astanait.edu.kz>
2024-02-25 20:44:12 +0600

    start 4assig

--- services/contact/internal/repository/contact_repository_impl.go
+++ services/contact/internal/repository/contact_repository_impl.go
@@ -1,5 +1,3 @@
-// internal/repository/contact_repository_impl.go
-
 package repository
 
 import (
@@ -16,14 +14,12 @@ func NewContactRepository(db *sql.DB) ContactRepository {
 }
 
 func (r *ContactRepositoryImpl) CreateContact(contact *domain.Contact) (*domain.Contact, error) {
-	// Реализация вставки контакта в базу данных
 	query := "INSERT INTO contacts (first_name, last_name, middle_name, phone_number) VALUES (?, ?, ?, ?)"
 	result, err := r.db.Exec(query, contact.FirstName, contact.LastName, contact.MiddleName, contact.PhoneNumber)
 	if err != nil {
 		return nil, err
 	}
 
-	// Получаем ID вставленной записи
 	contactID, err := result.LastInsertId()
 	if err != nil {
 		return nil, err
@@ -47,14 +43,12 @@ func (r *ContactRepositoryImpl) GetContactByID(contactID int) (*domain.Contact, 
 }
 
 func (r *ContactRepositoryImpl) CreateGroup(group *domain.Group) (*domain.Group, error) {
-	// Реализация вставки группы в базу данных
 	query := "INSERT INTO groups (name) VALUES (?)"
 	result, err := r.db.Exec(query, group.Name)
 	if err != nil {
 		return nil, err
 	}
 
-	// Получаем ID вставленной записи
 	groupID, err := result.LastInsertId()
 	if err != nil {
 		return nil, err
@@ -65,7 +59,6 @@ func (r *ContactRepositoryImpl) CreateGroup(group *domain.Group) (*domain.Group,
 }
 
 func (r *ContactRepositoryImpl) AddContactToGroup(contactID, groupID int) error {
-	// Реализация добавления контакта в группу в базе данных
 	query := "INSERT INTO contacts_groups (contact_id, group_id) VALUES (?, ?)"
 	_, err := r.db.Exec(query, contactID, groupID)
 	if err != nil {
@@ -76,7 +69,6 @@ func (r *ContactRepositoryImpl) AddContactToGroup(contactID, groupID int) error 
 }
 
 func (r *ContactRepositoryImpl) DeleteContact(contactID int) error {
-	// Реализация удаления контакта из базы данных
 	query := "DELETE FROM contacts WHERE id = ?"
 	_, err := r.db.Exec(query, contactID)
 	if err != nil {
@@ -87,7 +79,6 @@ func (r *ContactRepositoryImpl) DeleteContact(contactID int) error {
 }
 
 func (r *ContactRepositoryImpl) GetGroupByID(groupID int) (*domain.Group, error) {
-	// Реализация чтения группы из базы данных
 	query := "SELECT id, name FROM groups WHERE id = ?"
 	row := r.db.QueryRow(query, groupID)
 
@@ -101,7 +92,6 @@ func (r *ContactRepositoryImpl) GetGroupByID(groupID int) (*domain.Group, error)
 }
 
 func (r *ContactRepositoryImpl) UpdateContact(contact *domain.Contact) (*domain.Contact, error) {
-	// Реализация обновления контакта в базе данных
 	query := "UPDATE contacts SET first_name = ?, last_name = ?, middle_name = ?, phone_number = ? WHERE id = ?"
 	_, err := r.db.Exec(query, contact.FirstName, contact.LastName, contact.MiddleName, contact.PhoneNumber, contact.ID)
 	if err != nil {
